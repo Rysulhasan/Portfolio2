cf482583f863542f6c587cd2e106f959d38b4ca4
Vishvas Gohil <122722469+vishvasg14@users.noreply.github.com>
2024-11-21 21:38:45 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,8 +200,6 @@ fetchData("projects").then(data => {
 // <!-- contact section ends -->
 
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -211,8 +209,7 @@ s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
-</script>
-<!--End of Tawk.to Script-->
+
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
